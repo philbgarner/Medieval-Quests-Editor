7e4c6ca7a05ad955d652ece15978626d19abdc8e
Phil Garner <phil.b.garner@gmail.com>
2020-10-25 10:59:40 -0400

    Adding build folder, you can just open the html file in that folder to launch without installing npm.

---
+++ build/precache-manifest.f41215aa9860d2918a0fd307ee13e473.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0c7b4dde4ef081306c78afbe86d318bb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "997acd35e637881a1af6",
+    "url": "/static/css/main.77abfc1c.chunk.css"
+  },
+  {
+    "revision": "28b7bec8644541799af1",
+    "url": "/static/js/2.2ca201ef.chunk.js"
+  },
+  {
+    "revision": "9f9d9cb04e6570c1d63b5e28d6a524f7",
+    "url": "/static/js/2.2ca201ef.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "997acd35e637881a1af6",
+    "url": "/static/js/main.e45e0a6c.chunk.js"
+  },
+  {
+    "revision": "230330982fc0ae718767",
+    "url": "/static/js/runtime-main.778a3842.js"
+  }
+]);
